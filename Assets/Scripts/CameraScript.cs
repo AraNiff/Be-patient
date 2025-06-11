628a6f65320f054f9ac747e5033d9260ed41b5d8
AraN <102259330+AraNiff@users.noreply.github.com>
2025-06-11 21:46:22 +0300

    Fixed player movement bug

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -3,17 +3,27 @@ using System.Collections;
 
 public class CameraScript : MonoBehaviour
 {
-    public Transform transformCamera;
-    public Component mainCharacter;
+    public Transform target;
+    public float smoothSpeed = 2f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        mainCharacter = GameObject.FindGameObjectWithTag("MainCharacter").GetComponent<Transform>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void LateUpdate()
+    {
+        if (target != null)
+        {
+            Vector3 newPos = new Vector3(target.position.x, target.position.y, -10f);
+            transform.position = Vector3.Lerp(transform.position, newPos, smoothSpeed);
+        }
     }
 }
--- Assets/Scripts/SkeletonMovement.cs
+++ Assets/Scripts/SkeletonMovement.cs
@@ -1,27 +1,36 @@
 using UnityEngine;
 using System.Collections;
 using UnityEngine.InputSystem;
+using Unity.Mathematics;
 
 public class SkeletonMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
     public Rigidbody2D rigidBody2D;
+    public Transform character;
     private Vector2 movement;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rigidBody2D = GameObject.FindGameObjectWithTag("MainCharacter").GetComponent<Rigidbody2D>();
+        rigidBody2D.freezeRotation = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxis("Horizontal");
-        movement.y = Input.GetAxis("Vertical");
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        movement.Normalize();
     }
 
     private void FixedUpdate()
     {
+        // rigidBody2D.linearVelocity = movement * moveSpeed * Time.deltaTime;
+        //Vector2 movement2 = new Vector2(movement.x, movement.y);
+        //transform.Translate(movement2 * moveSpeed * Time.deltaTime);
+
+        //(rigidBody2D.position + movement * moveSpeed * Time.deltaTime);
         rigidBody2D.MovePosition(rigidBody2D.position + movement * moveSpeed * Time.deltaTime);
     }
 }
