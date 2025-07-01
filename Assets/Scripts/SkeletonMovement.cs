594a1c7c567f53ec36cc7417734e8e87e226a28e
AraN <102259330+AraNiff@users.noreply.github.com>
2025-07-01 15:04:25 +0300

    Walking and idle animations

--- Assets/Scripts/SkeletonMovement.cs
+++ Assets/Scripts/SkeletonMovement.cs
@@ -9,11 +9,16 @@ public class SkeletonMovement : MonoBehaviour
     public Rigidbody2D rigidBody2D;
     public Transform character;
     private Vector2 movement;
+    public SpriteRenderer spriteRenderer;
+
+    public Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rigidBody2D = GameObject.FindGameObjectWithTag("MainCharacter").GetComponent<Rigidbody2D>();
         rigidBody2D.freezeRotation = true;
+
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -26,11 +31,22 @@ public class SkeletonMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // rigidBody2D.linearVelocity = movement * moveSpeed * Time.deltaTime;
-        //Vector2 movement2 = new Vector2(movement.x, movement.y);
-        //transform.Translate(movement2 * moveSpeed * Time.deltaTime);
-
-        //(rigidBody2D.position + movement * moveSpeed * Time.deltaTime);
         rigidBody2D.MovePosition(rigidBody2D.position + movement * moveSpeed * Time.deltaTime);
+        if (movement.x != 0 || movement.y != 0)
+        {
+            animator.SetFloat("isWalking", 1);
+            if (movement.x < 0)
+            {
+                spriteRenderer.flipX = true;
+            }
+            else
+            {
+                spriteRenderer.flipX = false;
+            }
+        }
+        else
+        {
+            animator.SetFloat("isWalking", 0);
+        }
     }
 }
